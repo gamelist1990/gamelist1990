38c0f60206e30ff059b278559824663925312d24
gamelist <118247906+gamelist1990@users.noreply.github.com>
2023-02-23 15:04:40 +0900

    Create index.7bacaf5d.js

---
+++ block/assets/index.7bacaf5d.js
@@ -0,0 +1,16 @@
+import {T as o, C as t, b as a} from "./index.b2bcc769.js";
+import {P as s} from "./url.e5e1fed9.js";
+import {z as r, K as m, B as e, H as i} from "./index.1640afea.js";
+import "./BareClient.06787bc0.js";
+const b = {
+    Tab: o,
+    getTabs: r,
+    Protocol: s,
+    getProtocols: m,
+    Bookmark: e,
+    getBookmarks: i,
+    ContextItem: t,
+    bindIFrameMousemove: a
+};
+window.Velocity = b;
+export {b as default};

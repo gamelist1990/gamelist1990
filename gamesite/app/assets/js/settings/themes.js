3990c21a553ff91487c413d6184c7692942dd764
gamelist <118247906+gamelist1990@users.noreply.github.com>
2023-02-26 03:03:33 +0900

    Add files via upload

---
+++ gamesite/app/assets/js/settings/themes.js
@@ -0,0 +1,33 @@
+document.getElementById("Themes").onchange = function()
+{
+  if(this.value === "Classic") {
+  localStorage.setItem('Classic', 'on');
+	document.body.style.backgroundColor = "#111";
+	document.body.style.color = "#ffffff";
+	document.body.style.setProperty('--bg-c', "#111");
+	localStorage.setItem('Ocean', 'off');
+	window.localStorage.removeItem('Midnight');
+	window.localStorage.setItem('Ocean', 'off');
+	window.localStorage.removeItem('Ocean');
+}
+    if(this.value === "Ocean") {
+  localStorage.setItem('Ocean', 'on');
+	document.body.style.backgroundColor = "#162545";
+	document.body.style.color = "#ffffff";
+	document.body.style.setProperty('--bg-c', "#162545");
+	localStorage.setItem('classic', 'off');
+	window.localStorage.removeItem('classic');
+	window.localStorage.setItem('lava', 'off');
+	window.localStorage.removeItem('lava');
+}
+  if(this.value === "Midnight") {
+  localStorage.setItem('midnight', 'on');
+	document.body.style.backgroundColor = "#000";
+	document.body.style.color = "#ffffff";
+	document.body.style.setProperty('--bg-c', "#000");
+	localStorage.setItem('classic', 'off');
+	window.localStorage.removeItem('classic');
+	window.localStorage.setItem('lava', 'off');
+	window.localStorage.removeItem('lava');
+  }
+    };

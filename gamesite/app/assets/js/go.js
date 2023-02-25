6da45ef7c9d38ab5e5b65a00aa29ed4151b0fb2c
gamelist <118247906+gamelist1990@users.noreply.github.com>
2023-02-26 03:56:10 +0900

    Update go.js

--- gamesite/app/assets/js/go.js
+++ gamesite/app/assets/js/go.js
@@ -11,6 +11,6 @@ function go(value) {
         url = "https://" + url;
       //pass the encoded url to the second page
       sessionStorage.setItem("encodedUrl", __uv$config.encodeUrl(url));
-      location.href = "go.html";
+      location.href = "https://gamelist1990.github.io/gamelist1990/gamesite/app/go.html";
     });
 }
